bb7bbd2950da9e2c6f2eb2c95cdae3d68a7966f1
arvind zagade <arvindzagade@gmail.com>
2023-07-08 15:50:35 +0530

    Hyperparamter tuning

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,7 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -24,7 +25,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     """ This function will evaluate the performance of the model.
     """
     try:
@@ -35,9 +36,17 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
 
         for i in range(len(list(models))):
 
-            model = list(models.values())[i] # getting models one by one
+            model = list(models.values())[i] # getting models one by one from mode_trainer.py file
+            para = param[list(models.keys())[i]] # All parameters we have here from mode_trainer.py file
 
-            model.fit(X_train,y_train) # training  model
+            ## GridSearchCV
+            gs = GridSearchCV(model,para,cv= 3)#n_jobs=n_jobs,verbose=verbose,refit = refit)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            # model.fit(X_train,y_train) # training  model
 
             y_train_pred = model.predict(X_train)
 
